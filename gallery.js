10f8365534c5b6abaccaf68cae2ce97394670f26
luvlestudio <sowoojoo31@gmail.com>
2025-03-16 16:56:27 +0900

    555

--- gallery.js
+++ gallery.js
@@ -46,7 +46,7 @@ window.onload = () => {
     Kakao.Share.sendDefault({
       objectType: 'feed',
       content: {
-        title: '희철 ♥ 다빈의 모바일 청첩장',
+        title: '희철 ♥ 하나의 모바일 청첩장',
         description: '2025년 4월 17일 목요일♡',
         imageUrl: 'https://i.imgur.com/VcxASNj.png',
         imageWidth: 1200,
